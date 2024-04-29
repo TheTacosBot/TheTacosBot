e2d664a195ebce2feeea7024a359c514c9799418
Gabe Levasseur <11285350+GabeL7r@users.noreply.github.com>
2024-04-29 16:38:38 -0400

    feat: wip add drift detection

--- src/handlers/pull_request.py
+++ src/handlers/pull_request.py
@@ -5,33 +5,44 @@ from src.github import GitHub
 
 def pull_request_handler(config):
     token = os.environ.get("INPUT_GITHUB_TOKEN")
+    drift_detection_enabled = os.environ.get("INPUT_DRIFT_DETECTION") != ""
     github = GitHub(token)
     files_changed = github.pull_request_files_changed(False)
 
-    projects_to_run = config.get_projects_to_run(files_changed)
+    projects_to_run = config.get_projects_to_run(files_changed, drift_detection_enabled)
 
     # TODO: how to notify user if no projects have changed
 
     # TODO: add ability to trigger plans for modules (i.e. atlantis autoplan feature)
     # Iterate over the projects and execute them
-    for _, project in projects_to_run.items():
-        (deployment_id, is_this_pr) = github.project_has_pending_deployment(project.name)
+    if drift_detection_enabled:
+        ref = os.getenv("GITHUB_REF")
+        for _, project in projects_to_run.items():
+            inputs = {
+                'name': project.name,
+                **project.dict(),
+            }
+            print(f"Invoking {project.workflow} for project: {project.name}")
+            github.invoke_workflow_dispatch(project.workflow, ref, inputs)
+    else:
+        for _, project in projects_to_run.items():
+            (deployment_id, is_this_pr) = github.project_has_pending_deployment(project.name)
 
-        if deployment_id is not None and not is_this_pr:
-            return print("Another PR has a pending deployment")
+            if deployment_id is not None and not is_this_pr:
+                return print("Another PR has a pending deployment")
+            
+            # We need to replace the existing deployment with a new one since the
+            # sha and information changes.
+            if deployment_id is not None and is_this_pr:
+                print("Found previously existing deployment for this PR. Removing it.")
+                github.delete_deployment(deployment_id)
         
-        # We need to replace the existing deployment with a new one since the
-        # sha and information changes.
-        if deployment_id is not None and is_this_pr:
-            print("Found previously existing deployment for this PR. Removing it.")
-            github.delete_deployment(deployment_id)
-    
-        inputs = {
-            'name': project.name,
-            **project.dict(),
-        }
-        print(f"Creating Deployment for {project.name}")
-        deployment_id = github.create_deployment(project)
-        github.update_deployment_status(deployment_id, 'pending', f'Creating deployment for {project.name}')
-        print(f"Invoking {project.workflow} for project: {project.name}")
-        github.invoke_workflow_dispatch(project.workflow, github.head_branch, inputs)
+            inputs = {
+                'name': project.name,
+                **project.dict(),
+            }
+            print(f"Creating Deployment for {project.name}")
+            deployment_id = github.create_deployment(project)
+            github.update_deployment_status(deployment_id, 'pending', f'Creating deployment for {project.name}')
+            print(f"Invoking {project.workflow} for project: {project.name}")
+            github.invoke_workflow_dispatch(project.workflow, github.head_branch, inputs)
