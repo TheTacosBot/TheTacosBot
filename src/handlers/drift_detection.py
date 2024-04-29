d5fa3300b15d21b3cfefc6ed97e35f990be5e22a
Gabe Levasseur <11285350+GabeL7r@users.noreply.github.com>
2024-04-29 17:48:58 -0400

    debug

--- src/handlers/drift_detection.py
+++ src/handlers/drift_detection.py
@@ -6,11 +6,19 @@ from src.github import GitHub
 def drift_detection_handler(config):
     token = os.environ.get("INPUT_GITHUB_TOKEN")
     github = GitHub(token)
-    files_changed = subprocess.run(
-        'git ls-tree --full-tree -r --name-only HEAD',
-        shell=True,
-        capture_output=True,
-        check=True).stdout.decode('utf-8').split('\n')
+    try:
+        result = subprocess.run(
+            'git ls-tree --full-tree -r --name-only HEAD',
+            shell=True,
+            capture_output=True,
+            check=True
+        )
+        files_changed = result.stdout.decode('utf-8').split('\n')
+    except subprocess.CalledProcessError as e:
+        print("Error occurred while executing the command.")
+        print("Exit status:", e.returncode)
+        print("Output:", e.stdout.decode('utf-8'))
+        print("Error:", e.stderr.decode('utf-8'))
 
     projects_to_run = config.get_projects_to_run(files_changed)
 
