8f5538a9e05b62f199f3dbbadb215436116da4bb
Gabe Levasseur <11285350+GabeL7r@users.noreply.github.com>
2024-04-30 19:51:55 -0400

    test plan summary

--- src/handlers/drift_detection.py
+++ src/handlers/drift_detection.py
@@ -6,7 +6,6 @@ from src.github import GitHub
 def drift_detection_handler(config):
     token = os.environ.get("INPUT_GITHUB_TOKEN")
     github = GitHub(token)
-    print(os.environ.get("INPUT_FILES"))
     files_changed = os.environ.get("INPUT_FILES").split('|')
 
     projects_to_run = config.get_projects_to_run(files_changed)
--- src/handlers/pull_request.py
+++ src/handlers/pull_request.py
@@ -1,5 +1,4 @@
 import os
-import requests
 from src.github import GitHub
 
 
