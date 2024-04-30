e615b86240f5378fec62c4e5b9de7090708120b7
Gabe Levasseur <11285350+GabeL7r@users.noreply.github.com>
2024-04-29 21:09:04 -0400

    trigger

--- examples/gh_dir4/main.tf
+++ examples/gh_dir4/main.tf
@@ -1,25 +1,8 @@
-module "vpc" {
-  source = "cloudposse/vpc/aws"
-  # Cloud Posse recommends pinning every module to a specific version
-  # version     = "x.x.x"
-  namespace = "eg"
-  stage     = "test"
-  name      = "app"
+resource "aws_s3_bucket" "example" {
+  bucket_prefix = "my-tf-test-bucket6"
 
-  ipv4_primary_cidr_block = "10.0.0.0/16"
-
-  assign_generated_ipv6_cidr_block = false
-}
-
-module "dynamic_subnets" {
-  source = "cloudposse/dynamic-subnets/aws"
-  # Cloud Posse recommends pinning every module to a specific version
-  # version     = "x.x.x"
-  namespace          = "eg"
-  stage              = "test"
-  name               = "app"
-  availability_zones = ["us-west-2a","us-west-2b","us-west-2c"]
-  vpc_id             = module.vpc.vpc_id
-  igw_id             = [module.vpc.igw_id]
-  ipv4_cidr_block         = ["10.0.0.0/16"]
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
 }
